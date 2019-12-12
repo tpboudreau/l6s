5948c39f7c260ac1bde43750e4fa25ce25142c1f
TP Boudreau <tpboudreau@gmail.com>
2019-12-12 01:31:49 +0000

    some work on helm templates

--- build.d/device.d/build.sh
+++ build.d/device.d/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker build -t tpboudreau/librenms-device .
+#docker build -t tpboudreau/librenms-device .
+docker build -t tpboudreau/librenms-device-v2c .
 
---
+++ build.d/device.d/v3/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#docker build -t tpboudreau/librenms-v3device .
+docker build -t tpboudreau/librenms-device-v3 .
+
